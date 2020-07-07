1c94afa27f2cde2b972bb10d0affd931aa2a61e3
Ayaaz <aversi786@gmail.com>
2020-07-07 11:32:50 -0400

    next and vote button work, added functionality to start with random message not just after clicking next

--- Part1/anecdotes/src/index.js
+++ Part1/anecdotes/src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const App = (props) => {
+  const length = props.anecdotes.length
+  const [selected, setSelected] = useState(Math.floor(Math.random() * (length - 1)))
+  const [votes, setVotes] = useState(new Array(length).fill(0))
+  const handleVote = () => {
+    const newVotes = [...votes]
+    newVotes[selected] += 1
+    setVotes(newVotes)
+  }
+
+  const handleNext = () => {
+    setSelected(Math.floor(Math.random() * (length - 1)))
+  }
+
+  return (
+    <div>
+      {props.anecdotes[selected]}
+      <p>has {votes[selected]} votes</p>
+      <div>
+      <Button handleClick={handleVote} text="vote" />
+      <Button handleClick={handleNext} text="next anecdote" />
+      </div>
+    </div>
+  )
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App anecdotes={anecdotes} />,
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
