e32d0fdb3da124ec9554857c99ac2e1ac0b5d3ca
Ayaaz <aversi786@gmail.com>
2020-07-09 14:16:48 -0400

    step 3, numbe section

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -3,9 +3,11 @@ import Person from './components/Person'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567' }
   ]) 
   const [ newName, setNewName ] = useState('')
+
+  const [ newNumber, setNewNumber ] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
@@ -25,16 +27,24 @@ const App = () => {
 
     const nameObj = { 
       name: newName,
+      number: newNumber,
       id: newName
     }
 
     setPersons(persons.concat(nameObj))
     setNewName('')
+    setNewNumber('')
   }
+
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -43,6 +53,9 @@ const App = () => {
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
         </div>
         <div>
           <button type="submit">add</button>
