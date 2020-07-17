3cf9ae4489ab575e9e373ec1ec6f32a3e63fee98
Ayaaz <aversi786@gmail.com>
2020-07-17 13:41:00 -0400

    step one complete

--- Part2/dataforcountries/src/components/CountryDetail.js
+++ Part2/dataforcountries/src/components/CountryDetail.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import Language from './Language'
 
 const CountryDetail = ({country}) => {
     return (
-        <div>detail {country.name}</div>
+        <div>
+            <h1>{country.name}</h1>
+            <div>captial {country.capital}</div>
+            <div>population {country.population}</div>
+            <h2>languages</h2>
+            <ul>{country.languages.map((lang, index) => 
+                <Language name={lang.name} key={index}/>
+            )}</ul>
+            <img src={country.flag} width="200" height="100"/>
+        </div>
     )
 }
 
